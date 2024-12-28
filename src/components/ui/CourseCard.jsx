2407419c5a0589b445d0da6abddf5aefec430531
unknown <Ashik-Rahman100>
2024-12-28 12:40:37 +0600

    [feature] added card description modal

--- src/components/ui/CourseCard.jsx
+++ src/components/ui/CourseCard.jsx
@@ -1,3 +1,5 @@
+import CourseModal from "./CourseModal";
+
 /* eslint-disable react/prop-types */
 export default function CourseCard({ course }) {
   // console.log(course);
@@ -9,16 +11,34 @@ export default function CourseCard({ course }) {
       </figure>
       <div className="card-body">
         <div className="">
-          <div className="pb-2" >Published: <span className="badge badge-outline">{course?.created_at}</span></div>
-          <div className="pb-2" >Instructor: <span className="badge badge-outline">{course?.instructor_name}</span></div>
-          <div >Author: <span className="badge badge-outline">{course?.author?.name}</span></div>
-         
+          <div className="pb-2">
+            Published:{" "}
+            <span className="badge badge-outline">{course?.created_at}</span>
+          </div>
+          <div className="pb-2">
+            Instructor:{" "}
+            <span className="badge badge-outline">
+              {course?.instructor_name}
+            </span>
+          </div>
+          <div>
+            Author:{" "}
+            <span className="badge badge-outline">{course?.author?.name}</span>
+          </div>
         </div>
         <h2 className="card-title">
           {course?.title}
-          <div className={`badge bg-${course?.badge_color}-500  `}>{course?.badge_text}</div>
+          <div
+            style={{ backgroundColor: course?.badge_color }}
+            className={`badge   `}
+          >
+            {course?.badge_text}
+          </div>
         </h2>
-        <p>{course?.description}</p>
+        <p>{course?.description.slice(0,100)}</p>
+        <div className="card-actions">
+         <CourseModal course={course} />
+        </div>
       </div>
     </div>
   );
